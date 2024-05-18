f63caa4abda57c784f5be87334cc6421bffda3aa
Jonas Magalhães <jonasevmagalhaes@gmail.com>
2024-05-18 15:58:11 -0300

    Tipando eventos do interceptor

--- src/app/shared/interceptors/format-url/format-url.interceptor.ts
+++ src/app/shared/interceptors/format-url/format-url.interceptor.ts
@@ -4,6 +4,7 @@ import { Observable, tap } from 'rxjs';
 export function FormatUrlInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   return next(req).pipe(tap(event => {
     if(event.type === HttpEventType.Sent) {
+      const request = event as unknown as HttpRequest<any>;
       console.log("Formatou URL antes de enviar");
     }
   }));
--- src/app/shared/interceptors/generic-errors/generic-errors.interceptor.ts
+++ src/app/shared/interceptors/generic-errors/generic-errors.interceptor.ts
@@ -1,9 +1,11 @@
-import { HttpEvent, HttpEventType, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpEventType, HttpHandlerFn, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 
 export function GenericErrorInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   return next(req).pipe(tap(event => {
     if(event.type === HttpEventType.Response) {
+      const response = event as HttpResponse<any>;
+      console.log(response.status);
       console.log("Alerta de erro genérico ao responder");
     }
   }));
